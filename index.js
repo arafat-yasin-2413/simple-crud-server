eced4beb00d675378087973993f968a5e2a3331b
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-05-18 00:10:13 +0600

    55-6 Done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
 const cors = require("cors");
@@ -60,11 +60,14 @@ async function run() {
         })
 
 
-        app.delete('/users/:id', (req, res)=>{
+        app.delete('/users/:id', async(req, res)=>{
             // console.log(req.params);
-            
             const id = req.params.id;
-            console.log('to be deleted : ', id);
+            // console.log('to be deleted : ', id);
+
+            const query = {_id: new ObjectId(id)}
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
         })
 
 
